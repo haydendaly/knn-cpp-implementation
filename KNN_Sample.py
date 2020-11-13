1e00b4d7cb39ae155f382a6e6e683b6ea3a64134
Roman Bellisari <32945706+romanbell@users.noreply.github.com>
2020-11-13 17:59:12 -0500

    Simple

---
+++ KNN_Sample.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
+distances, indices = nbrs.kneighbors(X)
+
+print("Indices: \n", indices)
+print("")
+print("Distances: \n",  distances)
